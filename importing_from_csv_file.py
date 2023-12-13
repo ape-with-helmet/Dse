60d3d657d945159a6f4be4d5260485ec3aa2b5cf
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-12-13 09:58:21 +0530

    Update importing_from_csv_file.py

--- importing_from_csv_file.py
+++ importing_from_csv_file.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-dataset = pd.read_csv(r'C:\Users\SAHYADRI\Downloads\data.csv')#use the path of the csv file here
+dataset = pd.read_csv(r'C:\Users\SAHYADRI\Downloads\data.csv')
 dur=pd.DataFrame(dataset,columns=["Duration"])
 pls=pd.DataFrame(dataset,columns=["Pulse"])
 mpls=pd.DataFrame(dataset,columns=["Maxpulse"])
 cal=pd.DataFrame(dataset,columns=["Calories"])
-plt.plot(dur)
-plt.plot(pls)
-plt.plot(mpls)
-plt.plot(cal)
-plt.xlabel("Max Pulse")
-plt.ylabel("Calories")
+figure,axis=plt.subplots(1,2)
+axis[0].plot(df.corr())
+axis[0].set_title("Correlation Graph")
+axis[1].plot(dataset)
+axis[1].set_title("Full Graph")
 plt.legend(["Duration","Pulse","Max Pulse","Calories"])
 print(df.to_string()) 
