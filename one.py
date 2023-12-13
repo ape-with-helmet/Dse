8446350b5dd04b5cbda6e80c5c1ce4c53a91b6b5
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-12-13 09:35:56 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,1 +1,9 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
+guests=np.array([23,32,27,50,22,18,11,65])
+subs=np.array(["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"])
+plt.plot(subs,guests,marker='o',color='#6834E2')
+plt.xlabel("Month/Year")
+plt.ylabel("Visitors")
+plt.legend(['No.of Visitors'])
+plt.show()
