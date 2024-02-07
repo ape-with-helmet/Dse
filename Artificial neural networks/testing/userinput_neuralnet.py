efc2c78923c660688016158500b93a2ff60eb84e
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2024-02-07 04:58:32 +0000

    ff

---
+++ Artificial neural networks/testing/userinput_neuralnet.py
@@ -0,0 +1,50 @@
+import numpy as np
+from tqdm import tqdm
+x=np.array(([2,9],[1,5],[3,6]),dtype=float)
+y=np.array(([92],[86],[89]),dtype=float)
+x=x/np.amax(x,axis=0)
+y=y/100
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+def derivative(x):
+    return x*(1-x)
+epoch=5000
+lr=10
+input_neurons=2
+hidden_neurons=3
+output_neurons=1
+wh=np.random.uniform(size=(input_neurons,hidden_neurons))
+bh=np.random.uniform(size=(1,hidden_neurons))
+wout=np.random.uniform(size=(hidden_neurons,output_neurons))
+bout=np.random.uniform(size=(1,output_neurons))
+for j in tqdm(range(100)):
+    for i in range(epoch):
+        hinpl=np.dot(x,wh)
+        hinp=hinpl+bh
+        hlayer_act=sigmoid(hinp)
+        outinpl=np.dot(hlayer_act,wout)
+        outinp=outinpl+bout
+        output=sigmoid(outinp)
+        E0=y-output
+        outgrad=derivative(output)
+        d_output=E0*outgrad
+        EH=d_output.dot(wout.T)
+        hiddengrad=derivative(hlayer_act)
+        d_hiddenlayer=EH*hiddengrad
+        wout+=hlayer_act.T.dot(d_output)*lr
+        wh+=x.T.dot(d_hiddenlayer)*lr
+
+slep=int(input("Enter the sleep time: "))
+stud=int(input("Enter the study time: "))
+xinput=np.array([slep,stud],dtype=float)
+
+hinpl=np.dot(xinput,wh)
+hinp=hinpl+bh
+hlayer_act=sigmoid(hinp)
+outinpl=np.dot(hlayer_act,wout)
+outinp=outinpl+bout
+output=sigmoid(outinp)
+
+print("Input: \n"+str(x))
+print("Actual Output: \n"+str(y))
+print("Predicted output: \n",output)
